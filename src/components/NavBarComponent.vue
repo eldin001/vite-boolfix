ce7334052b46a68f3e8fd179bab203454af2bfc8
eldin001 <eldin.balje@icloud.com>
2024-04-17 12:09:56 +0200

    added components

---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>header</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NavBarComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
